1f620ca9e906d30897a622aedca124db2a460d62
tosuke <13393900+tosuke@users.noreply.github.com>
2023-03-23 18:34:27 +0900

    test: test with MiniSat

--- test/test_dpll.ml
+++ test/test_dpll.ml
@@ -1,31 +1,32 @@
 open Sat
 
-let dpll_case name level cnf expected =
+let dpll_case name level cnf =
   let open Alcotest in
   test_case name level (fun () ->
+      let expected =
+        match Minisat_solver.solve cnf with UNSAT -> false | SAT _ -> true
+      in
       let actual =
-        match Solver.dpll cnf with UNSAT -> false | SAT _ -> true
+        match Dpll2.solve cnf with UNSAT -> false | SAT _ -> true
       in
       check bool name expected actual)
 
 let cases =
   [
     dpll_case "SAT" `Quick
-      (Cnf.from_list
-         [
-           [ -1; -2 ];
-           [ -1; 3 ];
-           [ -3; -4 ];
-           [ 2; 4; 5 ];
-           [ -5; 6; -7 ];
-           [ 2; 7; 8 ];
-           [ -8; -9 ];
-           [ -8; 10 ];
-           [ 9; -10; 11 ];
-           [ -10; -12 ];
-           [ -11; 12 ];
-         ])
-      true;
+      [
+        [ -1; -2 ];
+        [ -1; 3 ];
+        [ -3; -4 ];
+        [ 2; 4; 5 ];
+        [ -5; 6; -7 ];
+        [ 2; 7; 8 ];
+        [ -8; -9 ];
+        [ -8; 10 ];
+        [ 9; -10; 11 ];
+        [ -10; -12 ];
+        [ -11; 12 ];
+      ];
   ]
 
 let () =
